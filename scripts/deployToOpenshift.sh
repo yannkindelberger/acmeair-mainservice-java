6fa526608ff9d64c9a85519aaf6d7851ced2ab52
Yigit <yigitpolatt@gmail.com>
2020-12-23 16:15:56 +0300

    fix bugs

--- scripts/deployToOpenshift.sh
+++ scripts/deployToOpenshift.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-MANIFESTS="/root/acmeair-manifests"
+MANIFESTS="$PWD/acmeair-manifests"
 CLUSTER_DOMAIN=`oc -n openshift-ingress-operator get ingresscontrollers -o jsonpath='{.items[].status.domain}'`
 ROUTE_HOST="acmeair.${CLUSTER_DOMAIN}"
 
@@ -24,54 +24,51 @@ oc project acme-air
 mkdir -p ${MANIFESTS}
 cd ${MANIFESTS}
 echo "Installing Acme-Air in acme-air namespace ..."
+
 # Main service
-curl -o acmeair-mainservice-route.yaml https://raw.githubusercontent.com/mmondics/acmeair-mainservice-java/master/manifests-openshift/acmeair-mainservice-route.yaml
-curl -o deploy-acmeair-mainservice-java.yaml https://raw.githubusercontent.com/mmondics/acmeair-mainservice-java/master/manifests-openshift/deploy-acmeair-mainservice-java.yaml
+curl -o acmeair-mainservice-route.yaml https://raw.githubusercontent.com/yigitpolat/acmeair-mainservice-java/master/manifests-openshift/acmeair-mainservice-route.yaml
+curl -o deploy-acmeair-mainservice-java.yaml https://raw.githubusercontent.com/yigitpolat/acmeair-mainservice-java/master/manifests-openshift/deploy-acmeair-mainservice-java.yaml
 
 # Auth service
-curl -o acmeair-authservice-route.yaml https://raw.githubusercontent.com/mmondics/acmeair-authservice-java/master/manifests-openshift/acmeair-authservice-route.yaml
-curl -o deploy-acmeair-authservice-java.yaml https://raw.githubusercontent.com/mmondics/acmeair-authservice-java/master/manifests-openshift/deploy-acmeair-authservice-java.yaml
+curl -o acmeair-authservice-route.yaml https://raw.githubusercontent.com/yigitpolat/acmeair-authservice-java/master/manifests-openshift/acmeair-authservice-route.yaml
+curl -o deploy-acmeair-authservice-java.yaml https://raw.githubusercontent.com/yigitpolat/acmeair-authservice-java/master/manifests-openshift/deploy-acmeair-authservice-java.yaml
 
 # Customer service
-curl -o acmeair-customerservice-route.yaml https://raw.githubusercontent.com/mmondics/acmeair-customerservice-java/master/manifests-openshift/acmeair-customerservice-route.yaml
-curl -o deploy-acmeair-customerservice-java.yaml https://raw.githubusercontent.com/mmondics/acmeair-customerservice-java/master/manifests-openshift/deploy-acmeair-customerservice-java.yaml
+curl -o acmeair-customerservice-route.yaml https://raw.githubusercontent.com/yigitpolat/acmeair-customerservice-java/master/manifests-openshift/acmeair-customerservice-route.yaml
+curl -o deploy-acmeair-customerservice-java.yaml https://raw.githubusercontent.com/yigitpolat/acmeair-customerservice-java/master/manifests-openshift/deploy-acmeair-customerservice-java.yaml
 
 # Flight service
-curl -o acmeair-flightservice-route.yaml https://raw.githubusercontent.com/mmondics/acmeair-flightservice-java/master/manifests-openshift/acmeair-flightservice-route.yaml
-curl -o deploy-acmeair-flightservice-java.yaml https://raw.githubusercontent.com/mmondics/acmeair-flightservice-java/master/manifests-openshift/deploy-acmeair-flightservice-java.yaml
+curl -o acmeair-flightservice-route.yaml https://raw.githubusercontent.com/yigitpolat/acmeair-flightservice-java/master/manifests-openshift/acmeair-flightservice-route.yaml
+curl -o deploy-acmeair-flightservice-java.yaml https://raw.githubusercontent.com/yigitpolat/acmeair-flightservice-java/master/manifests-openshift/deploy-acmeair-flightservice-java.yaml
 
 # Booking service
-curl -o acmeair-bookingservice-route.yaml https://raw.githubusercontent.com/mmondics/acmeair-bookingservice-java/master/manifests-openshift/acmeair-bookingservice-route.yaml
-curl -o deploy-acmeair-bookingservice-java.yaml https://raw.githubusercontent.com/mmondics/acmeair-bookingservice-java/master/manifests-openshift/deploy-acmeair-bookingservice-java.yaml
+curl -o acmeair-bookingservice-route.yaml https://raw.githubusercontent.com/yigitpolat/acmeair-bookingservice-java/master/manifests-openshift/acmeair-bookingservice-route.yaml
+curl -o deploy-acmeair-bookingservice-java.yaml https://raw.githubusercontent.com/yigitpolat/acmeair-bookingservice-java/master/manifests-openshift/deploy-acmeair-bookingservice-java.yaml
 
 # Update hostname in routes manifest
 echo "Patch hostname for routes"
-if [[ `grep -c ${ROUTE_HOST} ${MANIFESTS}/acmeair-mainservice-route.yaml` == 0 ]]
-then
-  sed -i "s@_HOST_@${ROUTE_HOST}@" ${MANIFESTS}/acmeair-mainservice-route.yaml
-fi
+sed -i.bak "s@_HOST_@${ROUTE_HOST}@" ${MANIFESTS}/acmeair-mainservice-route.yaml
+sed -i.bak "s@_HOST_@${ROUTE_HOST}@" ${MANIFESTS}/acmeair-authservice-route.yaml
+sed -i.bak "s@_HOST_@${ROUTE_HOST}@" ${MANIFESTS}/acmeair-customerservice-route.yaml
+sed -i.bak "s@_HOST_@${ROUTE_HOST}@" ${MANIFESTS}/acmeair-flightservice-route.yaml
+sed -i.bak "s@_HOST_@${ROUTE_HOST}@" ${MANIFESTS}/acmeair-bookingservice-route.yaml
 
-if [[ `grep -c ${ROUTE_HOST} ${MANIFESTS}/acmeair-authservice-route.yaml` == 0 ]]
-then
-  sed -i "s@_HOST_@${ROUTE_HOST}@" ${MANIFESTS}/acmeair-authservice-route.yaml
-fi
-
-if [[ `grep -c ${ROUTE_HOST} ${MANIFESTS}/acmeair-customerservice-route.yaml` == 0 ]]
-then
-  sed -i "s@_HOST_@${ROUTE_HOST}@" ${MANIFESTS}/acmeair-customerservice-route.yaml
-fi
-
-if [[ `grep -c ${ROUTE_HOST} ${MANIFESTS}/acmeair-flightservice-route.yaml` == 0 ]]
-then
-  sed -i "s@_HOST_@${ROUTE_HOST}@" ${MANIFESTS}/acmeair-flightservice-route.yaml
-fi
-
-if [[ `grep -c ${ROUTE_HOST} ${MANIFESTS}/acmeair-bookingservice-route.yaml` == 0 ]]
-then
-  sed -i "s@_HOST_@${ROUTE_HOST}@" ${MANIFESTS}/acmeair-bookingservice-route.yaml
-fi
 
 oc create -f ${MANIFESTS}
+
+# Update back to original routes manifest
+echo "Removing ${IMAGE_PREFIX}"
+sed -i.bak "s@${ROUTE_HOST}@_HOST_@" ${MANIFESTS}/acmeair-mainservice-route.yaml
+sed -i.bak "s@${ROUTE_HOST}@_HOST_@" ${MANIFESTS}/acmeair-authservice-route.yaml
+sed -i.bak "s@${ROUTE_HOST}@_HOST_@" ${MANIFESTS}/acmeair-customerservice-route.yaml
+sed -i.bak "s@${ROUTE_HOST}@_HOST_@" ${MANIFESTS}/acmeair-flightservice-route.yaml
+sed -i.bak "s@${ROUTE_HOST}@_HOST_@" ${MANIFESTS}/acmeair-bookingservice-route.yaml
+
+rm ${MANIFESTS}/acmeair-mainservice-route.yaml.bak
+rm ${MANIFESTS}/acmeair-authservice-route.yaml.bak
+rm ${MANIFESTS}/acmeair-customerservice-route.yaml.bak
+rm ${MANIFESTS}/acmeair-flightservice-route.yaml.bak
+rm ${MANIFESTS}/acmeair-bookingservice-route.yaml.bak
 
 echo "========================================================================"
 echo "Acme Air Deployment complete. You can access the application now."
